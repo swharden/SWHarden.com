2b930218cd11f4babd3f1b1ff9784edffcf0804b
Scott W Harden <swharden@gmail.com>
2024-12-08 20:44:08 -0500

    update resume

---
+++ dev/resume/index.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Resume - Scott W Harden</title>
+</head>
+
+<body style="margin: 0; padding: 0;">
+    <embed src="resume.pdf?<?php echo rand(); ?>" width="100%" style="height: 100vh;">
+</body>
+
+</html>
