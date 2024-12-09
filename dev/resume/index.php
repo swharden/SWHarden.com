e3f0ceca7f0f864cc9a14b0a68e4162813591064
Scott W Harden <swharden@gmail.com>
2024-12-09 11:09:45 -0500

    update resume

--- dev/resume/index.php
+++ dev/resume/index.php
@@ -6,14 +6,52 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Resume - Scott W Harden</title>
     <style>
-        ::-webkit-scrollbar {
-            display: none;
+        body {
+            font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+        }
+
+        a {
+            text-decoration: none;
+        }
+
+        a:hover {
+            text-decoration: underline;
+        }
+
+        .spinner {
+            display: inline-block;
+            width: 1%;
+            height: 10px;
+            border: 1px solid transparent;
+            border-top: 3px solid black;
+            border-radius: 50%;
+            animation: spin 1s linear infinite;
+            margin: 0 0.5rem;
+        }
+
+        @keyframes spin {
+            0% {
+                transform: rotate(0deg);
+            }
+
+            100% {
+                transform: rotate(360deg);
+            }
         }
     </style>
 </head>
 
-<body style="margin: 0; padding: 0;">
-    <embed src="https://drive.google.com/viewerng/viewer?embedded=true&url=https://swharden.com/resume/resume.pdf?<?php echo rand(); ?>" width="100%" style="height: 100vh;">
+<body>
+    <div style="text-align: center; margin: 3rem; font-size: 1.2rem;">
+        Forwarding to <a href="https://swharden.com/resume/resume.pdf?<?php echo rand(); ?>">resume.pdf</a>
+        <div class="spinner"></div>
+    </div>
+
+    <script>
+        window.setTimeout(function () {
+            window.location.href = "resume.pdf?<?php echo rand(); ?>";
+        }, 2000);
+    </script>
 </body>
 
 </html>
